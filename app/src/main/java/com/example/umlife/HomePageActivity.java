999517aca5a8b6ed5998ee15d7a99605a695e55b
tcr123 <chunrong1805@gmail.com>
2022-12-21 23:31:54 +0800

    fixing fragment bug issue

---
+++ app/src/main/java/com/example/umlife/HomePageActivity.java
@@ -0,0 +1,68 @@
+package com.example.umlife;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.RelativeLayout;
+import android.widget.Toolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class HomePageActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+    Toolbar toolbar;
+    RelativeLayout relativeLayout;
+
+    // show all post from all user
+    PostFragment postFragment = new PostFragment();
+
+    // show all ongoing event
+    EventListFragment eventListFragment = new EventListFragment();
+
+    // reward system
+    RewardSystemFragment rewardSystemFragment = new RewardSystemFragment();
+
+    // profile of the user
+    ProfileFragment profileFragment = new ProfileFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // search relative layout
+        relativeLayout = findViewById(R.id.Main);
+
+        // bottom navigation
+        bottomNavigationView = findViewById(R.id.bottomnavigation);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, postFragment).commit();
+
+        bottomNavigateListener();
+    }
+
+    private void bottomNavigateListener() {
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()) {
+//                    case R.id.home:
+//                        getSupportFragmentManager().beginTransaction().replace(R.id.container, postFragment).commit();
+//                        return true;
+//                    case R.id.event:
+//                        getSupportFragmentManager().beginTransaction().replace(R.id.container, eventListFragment).commit();
+//                        return true;
+//                    case R.id.reward:
+//                        getSupportFragmentManager().beginTransaction().replace(R.id.container, rewardSystemFragment).commit();
+//                        return true;
+//                    case R.id.profile:
+//                        getSupportFragmentManager().beginTransaction().replace(R.id.container, profileFragment).commit();
+//                        return true;
+                }
+                return false;
+            }
+        });
+    }
+}
