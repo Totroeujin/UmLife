c10ee4751b491dea01873223ce4261ae407e40fb
zianttt <ziant0927@gmail.com>
2023-01-02 12:04:36 +0800

    Rewards - Undone

--- app/src/main/java/com/example/umlife/RewardsAdapter.java
+++ app/src/main/java/com/example/umlife/RewardsAdapter.java
@@ -7,60 +7,40 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 import com.example.model.Reward;
 
 import java.util.Date;
 import java.util.List;
 
-public class RewardsAdapter extends RecyclerView.Adapter<RewardsAdapter.RewardView> {
-    private List<Reward> rewardList;
-    private FragmentActivity fragmentActivity;
+public class RewardsAdapter extends FragmentStateAdapter {
 
-    public class RewardView extends RecyclerView.ViewHolder{
-        TextView TVRewardName;
-        TextView TVRewardDescription;
-        ImageView IVRewardImage;
+    private List<Reward> rewards;
 
-        public RewardView(View view){
-            super(view);
-
-            TVRewardName = view.findViewById(R.id.TVRewardName);
-            TVRewardDescription = view.findViewById(R.id.TVRewardDescription);
-            IVRewardImage = view.findViewById(R.id.IVRewardImage);
-        }
+    public RewardsAdapter(Fragment fragment) {
+        super(fragment);
     }
 
-    public RewardsAdapter(FragmentActivity fragmentActivity, List<Reward> rewardList){
-        this.fragmentActivity = fragmentActivity;
-        this.rewardList = rewardList;
+    public RewardsAdapter(FragmentManager fragmentManager, Lifecycle lifecycle, List<Reward> rewards) {
+        super(fragmentManager, lifecycle);
+        this.rewards = rewards;
     }
-
 
     @NonNull
     @Override
-    public RewardView onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        //Inflate item.xml using LayoutInflator
-        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.reward_item, parent, false);
-        return new RewardView(itemView);
-    }
-
-    @Override
-    public void onBindViewHolder(final RewardView holder, final int position) {
-        Reward reward = rewardList.get(position);
-        holder.TVRewardName.setText(reward.getRewardName());
-        holder.TVRewardDescription.setText(reward.getRewardDescription());
-        // Picasso.get().load(reward.getImageUrl()).into(holder.IVTrendingImage);
+    public Fragment createFragment(int position) {
+        return new RewardListFragment(position);
     }
 
     @Override
     public int getItemCount() {
-        if(rewardList == null)
-            return 0;
-        int limit = 4;
-        return Math.min(rewardList.size(), limit);
+        return 2;
     }
 }
 
