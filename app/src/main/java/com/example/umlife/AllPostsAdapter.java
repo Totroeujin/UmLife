1852a3b94f369da2600426ef7afa46ddbc78abb3
tcr123 <chunrong1805@gmail.com>
2023-01-07 04:08:32 +0800

    Profile Function Work Done

---
+++ app/src/main/java/com/example/umlife/AllPostsAdapter.java
@@ -0,0 +1,111 @@
+package com.example.umlife;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.callbacks.QueryCompleteCallback;
+import com.example.model.Post;
+import com.example.model.UploadPost;
+import com.google.android.material.textfield.TextInputLayout;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+// import com.squareup.picasso.Picasso;
+
+public class AllPostsAdapter extends RecyclerView.Adapter<AllPostsAdapter.PostView> {
+    private List<Post> postsList;
+    private FragmentActivity fragmentActivity;
+
+    public class PostView extends RecyclerView.ViewHolder{
+        // Post details
+        CircleImageView IVPostUserImage;
+        TextView TVPostUsername;
+        TextView TVPostDetail;
+        ImageView IVPostImageUrl;
+        RecyclerView RVPostsList;
+
+        // Navigate to comment section
+//        TextView TVCommentNum;
+//        com.google.android.material.textfield.TextInputEditText ETComment;
+
+        public PostView(View view){
+            super(view);
+
+            IVPostUserImage = view.findViewById(R.id.IVPostUserImage);
+            TVPostUsername = view.findViewById(R.id.TVPostUsername);
+            TVPostDetail = view.findViewById(R.id.TVPostDetail);
+            IVPostImageUrl = view.findViewById(R.id.IVPostImageUrl);
+
+//            TVCommentNum = view.findViewById(R.id.TVCommentNum);
+//            ETComment = view.findViewById(R.id.ETComment);
+
+            // Set onClickListener to navigate to comment page and open keyboard
+//            TVCommentNum.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    int pos = getAdapterPosition();
+//                    if(pos > RecyclerView.NO_POSITION){
+//                        CommentFragment commentFragment = new CommentFragment();
+//                        commentFragment.setCurrentPost(postsList.get(pos), fragmentActivity);
+//                        fragmentActivity.getSupportFragmentManager().beginTransaction().replace(R.id.container, commentFragment).addToBackStack(null).commit();
+//                    }
+//                }
+//            });
+
+//            ETComment.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    int pos = getAdapterPosition();
+//                    if(pos > RecyclerView.NO_POSITION){
+//                        CommentFragment commentFragment = new CommentFragment();
+//                        commentFragment.setCurrentPost(postsList.get(pos), fragmentActivity);
+//                        fragmentActivity.getSupportFragmentManager().beginTransaction().replace(R.id.container, commentFragment).addToBackStack(null).commit();
+//                    }
+//                }
+//            });
+
+        }
+    }
+
+    public AllPostsAdapter(FragmentActivity fragmentActivity, List<Post> postsList){
+        this.fragmentActivity = fragmentActivity;
+        this.postsList = postsList;
+    }
+
+
+    @NonNull
+    @Override
+    public PostView onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //Inflate item.xml using LayoutInflator
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.post_item, parent, false);
+        return new PostView(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(final PostView holder, final int position) {
+        Post post = postsList.get(position);
+        holder.TVPostUsername.setText(post.getPostUsername());
+        holder.TVPostDetail.setText(post.getPostDetail());
+        Picasso.get().load(post.getPostUserImageUrl()).into(holder.IVPostUserImage);
+//        Picasso.get().load(post.getPostImageUrl()).into(holder.IVPostImageUrl);
+    }
+
+    @Override
+    public int getItemCount() {
+        if(postsList == null)
+            return 0;
+        int limit = 1;
+        return Math.min(postsList.size(), limit);
+    }
+}
--- app/src/main/java/com/example/umlife/ProfileFragment.java
+++ app/src/main/java/com/example/umlife/ProfileFragment.java
@@ -1,7 +1,5 @@
 package com.example.umlife;
 
-import static android.content.Context.MODE_PRIVATE;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
@@ -9,22 +7,33 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.callbacks.QueryCompleteCallback;
+import com.example.model.Post;
 import com.example.model.UserInfo;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.annotation.Nullable;
 
@@ -99,26 +108,96 @@ public class ProfileFragment extends Fragment{
     Button editProfile;
 
     //ProfileImage
-    CircleImageView profilePicture;
+    ImageView profilePicture;
 
     //Database ref
     FirebaseFirestore firestore;
 
+    RecyclerView allProfilePostList;
+    LinearLayoutManager VerticalLayout;
+    PostsListAdapter postsListAdapter;
+    List<Post> postsList = new ArrayList<>();
+    RecyclerView.LayoutManager PostsListRVLayoutManager;
+    FirebaseFirestore db;
 
     //Creating View
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_profile, container, false);
-        //this is the Testing fragment clickable site
+
+        QueryCompleteCallback queryCompleteCallback = new QueryCompleteCallback() {
+            @Override
+            public void onQueryComplete(List postList) {
+                postsListAdapter = new PostsListAdapter(getActivity(), postsList);
+                allProfilePostList.setAdapter(postsListAdapter);
+            }
+        };
 
         // Inflate the layout for this fragment
-        return v;
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        allProfilePostList = view.findViewById(R.id.allProfilePostList);
+        db = FirebaseFirestore.getInstance();
+
+        db.collection("posts").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                if(!queryDocumentSnapshots.isEmpty()) {
+                    List<DocumentSnapshot> documentSnapshots = queryDocumentSnapshots.getDocuments();
+                    for(DocumentSnapshot d: documentSnapshots) {
+                        // Log.d("Post document", d.toString());
+                        Post post = d.toObject(Post.class);
+                        post.setPostId(d.getId());
+
+                        db.collection("users").document(post.getPostUserId()).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                            @Override
+                            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                                if(documentSnapshot.exists()) {
+                                    UserInfo userInfo = documentSnapshot.toObject(UserInfo.class);
+                                    Log.d("User post image: ", post.getPostImageUrl());
+                                    post.setPostUserImageUrl(userInfo.getProfileImage());
+                                    post.setPostUsername(userInfo.getUsername());
+                                    // Add post to post list
+                                    postsList.add(post);
+                                    queryCompleteCallback.onQueryComplete(postsList);
+                                    Log.d("Postlist size 1: ", String.valueOf(postsList.size()));
+                                }
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(getContext(), "Fail to get post user data", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                    postsListAdapter.notifyDataSetChanged();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getContext(), "Fail to get data", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Log.d("Postlist size: ", String.valueOf(postsList.size()));
+        PostsListRVLayoutManager = new LinearLayoutManager(getActivity().getApplicationContext());
+        postsListAdapter = new PostsListAdapter(getActivity(), postsList);
+        VerticalLayout = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+
+        allProfilePostList.setLayoutManager(PostsListRVLayoutManager);
+        allProfilePostList.setItemAnimator(new DefaultItemAnimator());
+        allProfilePostList.setAdapter(postsListAdapter);
+        allProfilePostList.setLayoutManager(VerticalLayout);
+
+        return view;
     }
 
     //View object to change
     TextView username;
     TextView email;
+    TextView profileDes;
+    TextView profileBio;
 
     //View complete created
     @Override
@@ -132,6 +211,8 @@ public class ProfileFragment extends Fragment{
             editProfile = view.findViewById(R.id.editProfile);
             profilePicture = view.findViewById(R.id.profilePageImage);
             myReview = view.findViewById(R.id.myReviewIcon);
+            profileBio = view.findViewById(R.id.profileBio);
+            profileDes = view.findViewById(R.id.profileDescription);
 
             //get string from firestore
             firestore = FirebaseFirestore.getInstance();
@@ -145,6 +226,16 @@ public class ProfileFragment extends Fragment{
                             //Toast.makeText(getActivity(),temp.toString(),Toast.LENGTH_LONG).show();
                             Picasso.get().load(temp).into(profilePicture);
                             //profilePicture.setImageURI(temp);
+                        }
+                        if(document.getString("description") != null){
+                            profileDes.setText(document.getString("description"));
+                        }else{
+                            profileDes.setText("");
+                        }
+                        if(document.getString("bio") != null){
+                            profileBio.setText(document.getString("bio"));
+                        }else{
+                            profileBio.setText("");
                         }
                     }
                 }
