8345ae24ba44bf04420cf4271b1d26edb9e72ce3
Sherwynd Liew <sherwynd123@gmail.com>
2023-01-08 15:53:08 +0800

    Badge point added and implemented

--- app/src/main/java/com/example/profile/Badge.java
+++ app/src/main/java/com/example/profile/Badge.java
@@ -2,12 +2,20 @@ package com.example.profile;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.model.UserInfo;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import com.example.umlife.R;
 
@@ -26,6 +34,14 @@ public class Badge extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+
+    FirebaseFirestore db;
+    TextView userPoint;
+
+    TextView statusgold;
+    TextView statusplat;
+    TextView statusmas;
 
     public Badge() {
         // Required empty public constructor
@@ -75,6 +91,50 @@ public class Badge extends Fragment {
             public void onClick(View view) {
                 getActivity().onBackPressed();
             }
+        });
+
+        userPoint = view.findViewById(R.id.userPoint);
+        statusgold = view.findViewById(R.id.statusgold);
+        statusplat = view.findViewById(R.id.statusplat);
+        statusmas = view.findViewById(R.id.statusmas);
+
+        SetRewardPoints();
+
+
+    }
+
+    private void SetRewardPoints() {
+        db = FirebaseFirestore.getInstance();
+        UserInfo userInfo = (UserInfo) getActivity().getIntent().getSerializableExtra("userInfo");
+        db.collection("users").document(userInfo.getUuid()).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if(task.isSuccessful()){
+                    DocumentSnapshot document = task.getResult();
+                    int temp = Integer.parseInt(document.getString("points"));
+                    userPoint.setText(temp +" Points");
+                    if(temp>=1000 && temp<=5000){
+                        statusgold.setText("Status : achieved");
+                        statusplat.setText("Status : Not achieved");
+                        statusmas.setText("Status : Not achieved");
+                    }
+                    else if(temp>=5000 && temp<=10000){
+                        statusgold.setText("Status : achieved");
+                        statusplat.setText("Status : achieved");
+                        statusmas.setText("Status : Not achieved");
+                    }
+                    else if(temp>=10000){
+                        statusgold.setText("Status : achieved");
+                        statusplat.setText("Status : achieved");
+                        statusmas.setText("Status : achieved");
+                    }else{
+                            statusgold.setText("Status : Not achieved");
+                            statusplat.setText("Status : Not achieved");
+                            statusmas.setText("Status : Not achieved");
+                    }
+                }
+            }
+
         });
     }
 }
