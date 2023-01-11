a2475599116e7eaf8813705762c7c7627aa1da7c
tcr123 <chunrong1805@gmail.com>
2023-01-11 20:38:56 +0800

    small fix bug

--- app/src/main/java/com/example/umlife/SplashBackground.java
+++ app/src/main/java/com/example/umlife/SplashBackground.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.example.auth.LoginActivity;
+
 public class SplashBackground extends AppCompatActivity {
 
     Handler h = new Handler();;
